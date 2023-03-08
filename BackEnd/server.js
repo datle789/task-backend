2d573942d99803286bd8a4ccee44bc3c9e3c97b7
Leanh <datletb789@gmail.com>
2023-03-08 23:32:33 +0700

    push

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -15,7 +15,9 @@ const app = express();
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use(cors())
+app.use(cors({
+    origin: ["http://localhost:3000/", "http://task-app.onrender.com"],
+}))
 
 app.use("/api/tasks", route)
 
