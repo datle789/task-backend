26fd0c7ae19467289d44baafa388ab173d8875df
Leanh <datletb789@gmail.com>
2023-03-09 17:15:27 +0700

    add

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(cors({
-    origin: ["http://localhost:3000", "https://task-app.onrender.com"],
+    origin: ["https://task-app.onrender.com"],
 }))
 
 app.use("/api/tasks", route)
